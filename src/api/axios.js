56d3fdd1e8a28f06791fa6ed152f27eade8ebac4
qi chaoqiang <chaoqiang.qi@kongapi.com>
2019-12-02 19:18:17 +0800

    价格&ui改版

--- src/api/axios.js
+++ src/api/axios.js
@@ -14,9 +14,9 @@ axios.defaults.timeout = 100000
 // 测试地址（内网)
 // axios.defaults.baseURL = 'http://credit-api.int.anniu-tech.com'
 // 线上环境地址
-// axios.defaults.baseURL = 'https://good-lawyer-api.caishuiyu.com'
+axios.defaults.baseURL = 'https://good-lawyer-api.caishuiyu.com'
 // 本地环境地址
-axios.defaults.baseURL = 'http://172.100.8.116:8080/credit_api_war_exploded'
+// axios.defaults.baseURL = 'http://172.100.8.116:8080/credit_api_war_exploded'
 
 axios.interceptors.request.use((config) => {
   // 在发送请求之前做某件事
